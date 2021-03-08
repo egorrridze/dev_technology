6f0ed5c7dedbe6900fb3ea7905182e06afb2ef5a
SmokedKoala <k.azarov@inbox.ru>
2021-03-08 21:00:17 +0300

    Spinner
    Spinner works!

--- app/src/main/java/ru/practice/dev_technology/MainActivity.java
+++ app/src/main/java/ru/practice/dev_technology/MainActivity.java
@@ -3,17 +3,20 @@ package ru.practice.dev_technology;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Spinner choice1;
-    Spinner choice2;
+    Spinner choice1, choice2;
     Button convertButton;
     EditText inputNum;
+    View.OnClickListener clickListener;
 
 //    заглушка
     String[] data = {"one", "two", "three", "four", "five"};
@@ -23,15 +26,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        clickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()){
+                    case R.id.convert_button:
+                        Toast toast = Toast.makeText(getApplicationContext(),
+                                "It works!",
+                                Toast.LENGTH_SHORT);
+                        toast.show();
+                        break;
+                }
+            }
+        };
+
 //        адаптеры для списков
-//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.activity_main,data);
-//        adapter.setDropDownViewResource(R.layout.activity_main);
+        ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<>(
+                this, android.R.layout.simple_spinner_item, data);
+
+        spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
 
         choice1 = findViewById(R.id.choice1);
         choice2 = findViewById(R.id.choice2);
-//        choice1.setAdapter(adapter);
-//        choice2.setAdapter(adapter);
+        choice1.setAdapter(spinnerArrayAdapter);
+        choice2.setAdapter(spinnerArrayAdapter);
         convertButton = findViewById(R.id.convert_button);
+        convertButton.setOnClickListener(clickListener);
         inputNum = findViewById(R.id.number_input);
+
+
     }
+
 }
