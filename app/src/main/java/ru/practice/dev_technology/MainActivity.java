5d8ffba0b512d5a56c0be347962d05bc0614fa15
SmokedKoala <k.azarov@inbox.ru>
2021-03-30 20:47:32 +0300

    add: navigation menu
    markup of navigation menu
    toolbar is needed to be add

--- app/src/main/java/ru/practice/dev_technology/MainActivity.java
+++ app/src/main/java/ru/practice/dev_technology/MainActivity.java
@@ -7,32 +7,41 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 /** Represents main converter activity.
  * @author SmokedKoala
  * @version 0.0.1
  * @since 0.0.1
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     /**Spinners to choose value types from */
     Spinner choice1, choice2;
-
     /**Button for starting converting process */
     Button convertButton;
-
     /**Field for number input*/
     EditText inputNum;
-
-    /**ClickListener to process user clicks */
-    View.OnClickListener clickListener;
-
     /**Test array */
     String[] data = {"one", "two", "three", "four", "five"};
+    /**Adapters for spinners */
+    ArrayAdapter<String> spinnerArrayAdapter;
+    /**Drawer layout for menu */
+    DrawerLayout drawerLayout;
+    /**Menu */
+    NavigationView navigationView;
+    /**Menu */
+    ImageButton menuButton;
+
 
     /**
      * create main converter activity
@@ -44,21 +53,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        clickListener = v -> {
-            switch (v.getId()){
-                case R.id.convert_button:
-                    Toast toast = Toast.makeText(getApplicationContext(),
-                            "It works!",
-                            Toast.LENGTH_SHORT);
-                    toast.show();
-                    break;
-            }
-        };
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigation_view);
+        menuButton = findViewById(R.id.imageButton);
 
-//        адаптеры для списков
-        ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<>(
-                this, android.R.layout.simple_spinner_item, data);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this,
+                drawerLayout,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
 
+        spinnerArrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item,
+                data);
         spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
 
         choice1 = findViewById(R.id.choice1);
@@ -66,11 +71,22 @@ public class MainActivity extends AppCompatActivity {
         choice1.setAdapter(spinnerArrayAdapter);
         choice2.setAdapter(spinnerArrayAdapter);
         convertButton = findViewById(R.id.convert_button);
-        convertButton.setOnClickListener(clickListener);
+        convertButton.setOnClickListener(this);
         inputNum = findViewById(R.id.number_input);
         inputNum.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
 
 
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.convert_button:
+                Toast toast = Toast.makeText(getApplicationContext(),
+                        "It works!",
+                        Toast.LENGTH_SHORT);
+                toast.show();
+                break;
+        }
+    }
 }
