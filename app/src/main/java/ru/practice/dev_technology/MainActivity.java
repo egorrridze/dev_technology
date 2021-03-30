24c5f4e3d7865c92101ab5b3ed40567072273c25
SmokedKoala <k.azarov@inbox.ru>
2021-03-30 22:08:47 +0300

    upd: navigation menu
    navigationMenuItemClickListener added
    fix bug with closing app

--- app/src/main/java/ru/practice/dev_technology/MainActivity.java
+++ app/src/main/java/ru/practice/dev_technology/MainActivity.java
@@ -3,6 +3,7 @@ package ru.practice.dev_technology;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.text.InputType;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -12,18 +13,20 @@ import android.widget.Spinner;
 import android.widget.Toast;
 import android.widget.Toolbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.material.navigation.NavigationView;
 
 /** Represents main converter activity.
  * @author SmokedKoala
- * @version 0.0.1
+ * @version 0.0.2
  * @since 0.0.1
  */
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, NavigationView.OnNavigationItemSelectedListener {
 
     /**Spinners to choose value types from */
     Spinner choice1, choice2;
@@ -35,12 +38,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     String[] data = {"one", "two", "three", "four", "five"};
     /**Adapters for spinners */
     ArrayAdapter<String> spinnerArrayAdapter;
-    /**Drawer layout for menu */
+    /**Drawer layout for navigation view */
     DrawerLayout drawerLayout;
-    /**Menu */
+    /**Navigation View for menu */
     NavigationView navigationView;
-    /**Menu */
-    ImageButton menuButton;
 
 
     /**
@@ -55,15 +56,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         drawerLayout = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.navigation_view);
-        menuButton = findViewById(R.id.imageButton);
 
-        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this,
-                drawerLayout,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
-        drawerLayout.addDrawerListener(actionBarDrawerToggle);
-        actionBarDrawerToggle.syncState();
-
-        spinnerArrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item,
-                data);
+        spinnerArrayAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, data);
         spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
 
         choice1 = findViewById(R.id.choice1);
@@ -75,9 +70,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         inputNum = findViewById(R.id.number_input);
         inputNum.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
 
-
+        navigationMenuCreation();
     }
 
+    /**
+     * controls what application have to do if some element was pressed
+     * @param v View of the Activity
+     */
     @Override
     public void onClick(View v) {
         switch (v.getId()){
@@ -88,5 +87,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 toast.show();
                 break;
         }
+    }
+
+    /**
+     * controls what application have to do if back is pressed when menu is open
+     */
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else {
+        super.onBackPressed();
+        }
+    }
+
+    /**
+     * controls what application have to do if some menu element was pressed
+     * @param item menu item
+     */
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return true;
+    }
+
+
+    /**
+     * creates toggle and set NavigationItemSelectedListener for NavigationView
+     */
+    public void navigationMenuCreation(){
+        navigationView.bringToFront();
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this,
+                drawerLayout,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
     }
 }
